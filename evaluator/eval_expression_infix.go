19be79ddaecfc2fa65dac8f447cfd07c84817715
Sattvik Chakravarthy <sattvik@gmail.com>
2020-10-06 19:09:15 +0530

    Removing eval engine.

--- evaluator/eval_expression_infix.go
+++
@@ -1,9 +0,0 @@
-package evaluator
-
-import (
-	"github.com/dreblang/core/object"
-)
-
-func evalInfixExpression(operator string, left, right object.Object) object.Object {
-	return left.InfixOperation(operator, right)
-}
