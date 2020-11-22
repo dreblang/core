9ff01ecbbaadbd39659e2991ff9e75c54135c93b
Sattvik Chakravarthy <sattvik@gmail.com>
2020-11-22 17:54:03 +0530

    Updating tests.

--- tests.sh
+++ tests.sh
@@ -6,4 +6,7 @@ echo "Parser tests..."
 go test -cover -coverprofile=parser.out ./parser/... # && go tool cover -html=parser.out
 
 echo "AST tests..."
-go test -cover -coverprofile=ast.out ./ast/... && go tool cover -html=ast.out
+go test -cover -coverprofile=ast.out ./ast/... # && go tool cover -html=ast.out
+
+echo "Object tests..."
+go test -cover -coverprofile=object.out ./object/... # && go tool cover -html=object.out
